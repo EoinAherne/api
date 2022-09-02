bfa4ad1af963dbb2ce048598fa84c1a4c9ab4aa6
EoinAherne <aherneeoin88@gmail.com>
2022-09-02 13:26:06 +0000

    add post requests

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,15 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+asynv function postForm(e) {
+    const form = new FormData(document.getElementById('checksform'));
+
+    for (let e of form.entries()) {
+        console.log(e)
+    }
+}
 
 async function getStatus(e) {
 
@@ -13,7 +22,21 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
     }
+
+}
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 
 }
